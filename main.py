4d888c1b847b9ddfe9d6b8a36e0232e4871b5eec
Arttu Kuitunen <arsaku@utu.fi>
2024-03-26 10:25:11 +0200

    main.py and short documentation

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/another")
+async def root():
+    return {"message": "Another one!"}
+@app.post("/")
+async def root():
+    return {"message": "Post message!"}
