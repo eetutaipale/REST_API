aad74268bfb131bd0e307073d3b6e261f7f3051d
Arttu Kuitunen <arsaku@utu.fi>
2024-03-27 21:30:09 +0200

    Added __init__ and database.py files. Database connection established and data queries work. Next add remove and modify functions. Also connection to web-site with FastAPI should be developed.

---
+++ database.py
@@ -0,0 +1,36 @@
+
+from sqlalchemy import create_engine, Table, select, MetaData
+# from sqlalchemy.orm import sessionmaker -> when taking ORM into use, use sessions
+import time
+#import pandas as pd
+
+# Log in credentials and connection URL-string as DATABASE_CONNECTION
+SERVER_IP="34.88.224.107"
+DRIVER="ODBC+Driver+17+for+SQL+Server"
+DATABASE="TestDB"
+UID="Arttu"
+PWD="password"
+DATABASE_CONNECTION=f"mssql+pyodbc://{UID}:{PWD}@{SERVER_IP}/{DATABASE}?driver={DRIVER}"
+
+
+# Creating an engine object to connect to database
+engine = create_engine(DATABASE_CONNECTION)
+metadata = MetaData()
+
+# Using the engine object for straight connection to DB
+connection = engine.connect()
+print(f"Conncetion opened to {DATABASE} @ {SERVER_IP}")
+
+try:
+    result = connection.execute(select(Table('test_data', metadata, autoload_with=engine)))
+    rows = result.fetchall()
+    for row in rows:
+        print(row)
+
+except Exception as e:
+    print("Error:", e)
+time.sleep(1)
+
+print("Closing connection...")
+connection.close()
+
