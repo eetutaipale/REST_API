05d4ae5de75956f8aa7f475924361b2fade33faa
unknown <eetaip@utu.fi>
2024-04-03 11:49:36 +0300

    added fecth from api and saving it to a variable

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import create_engine, Table, select, MetaData
+from sqlalchemy import Column, create_engine, Table, select, MetaData
 from dotenv import load_dotenv
 import os
 # from sqlalchemy.orm import sessionmaker -> when taking ORM into use, use sessions
@@ -7,44 +7,56 @@ import time
 import logging
 
 #import pandas as pd
+def save_to_database(stock_data_list):
+    load_dotenv()
+    metadata = MetaData()
 
-load_dotenv()
-metadata = MetaData()
+    # Log in credentials and connection URL-string as DATABASE_CONNECTION
+    SERVER_IP = os.getenv("SERVER_IP")
+    DRIVER = os.getenv("DRIVER")
+    DATABASE = os.getenv("DATABASE")
+    UID = os.getenv("UID")
+    PWD = os.getenv("PWD")
+    DATABASE_CONNECTION = f"mssql+pyodbc://{UID}:{PWD}@{SERVER_IP}/{DATABASE}?driver={DRIVER}"
 
-# Log in credentials and connection URL-string as DATABASE_CONNECTION
-SERVER_IP = os.getenv("SERVER_IP")
-DRIVER = os.getenv("DRIVER")
-DATABASE = os.getenv("DATABASE")
-UID = os.getenv("UID")
-PWD = os.getenv("PWD")
-DATABASE_CONNECTION = f"mssql+pyodbc://{UID}:{PWD}@{SERVER_IP}/{DATABASE}?driver={DRIVER}"
+    # Set up logging configuration
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        filename='database.log',
+                        filemode='a')
 
-# Set up logging configuration
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='database.log',
-                    filemode='a')
+    # Creating an engine object to connect to database
+    engine = create_engine(DATABASE_CONNECTION)
 
-# Creating an engine object to connect to database
-engine = create_engine(DATABASE_CONNECTION)
+    # Using the engine object for straight connection to DB
+    connection = engine.connect()
+    print(f"Conncetion opened to {DATABASE} @ {SERVER_IP}")
+    stock_data_table = Table('stock_data', metadata,
+        Column('id', int, primary_key=True),
+        Column('name', str),
+        Column('price', str),
+        Column('volume', str),
+        Column('previous_close_price', str)
+    )
+    # SQL query with SQLAlchemy methods
+    if not metadata.tables.get('stock_data'):
+        # Create the table if it doesn't exist
+        stock_data_table.create()
+        print("Table 'stock_data' created")
 
-# Using the engine object for straight connection to DB
-connection = engine.connect()
-print(f"Conncetion opened to {DATABASE} @ {SERVER_IP}")
+        for stock_data in stock_data_list:
+            stock_data_table.insert().values(
+                name=stock_data['name'],
+                price=str(stock_data['price']),
+                volume=str(stock_data['volume']),
+                previous_close_price=str(stock_data['previous_close_price'])
+            ).execute()
+        
 
-# SQL query with SQLAlchemy methods
-try:
-    result = connection.execute(select(Table('test_data', metadata, autoload_with=engine)))
+    time.sleep(1)
 
-    rows = result.fetchall()
-    for row in rows:
-        print(row)
+    print("Closing connection...")
+    connection.close()
 
-except Exception as e:
-    logging.error("An error occurred: ", exc_info=True)
-    print("Error:", e)
-time.sleep(1)
 
-print("Closing connection...")
-connection.close()
 
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, HTTPException
+import requests
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+load_dotenv()
+TOK_API_TOKEN = os.getenv("TOK_API_TOKEN")
+def get_and_print_stock_quotes():
+    try:
+        url = f"https://api.stockdata.org/v1/data/quote?symbols=AAPL,TSLA,MSFT&api_token={TOK_API_TOKEN}"
 
+        response = requests.get(url)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+        if response.status_code == 200:
+            quotes_data = json.loads(response.text)
 
-@app.get("/another")
-async def root():
-    return {"message": "Another one!"}
+            if 'data' in quotes_data:
+                stock_data = quotes_data['data']
+                stock_info_list = []
+                # Print out the fetched data
+                print(f"Fetched stock quotes for {len(stock_data)} stocks:")
 
-@app.post("/")
-async def root():
-    return {"message": "Post message!"}
+                for stock in stock_data:
+                    stock_info = {
+                        "name": stock['name'],
+                        "price": stock['price'],
+                        "volume": stock['volume'],
+                        "previous_close_price": stock['previous_close_price']
+                    }
+                    stock_info_list.append(stock_info)
+                    print(stock_info_list)
+
+            return stock_info_list
+
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch stock quotes")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+@app.get("/get_stock_quotes")
+async def quotes():
+    stock = get_and_print_stock_quotes()
+    return {"stock data": stock}
+    
