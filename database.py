6e2e1589def205938417fa088fa402d18f64db11
Arttu Kuitunen <arsaku@utu.fi>
2024-04-05 12:24:29 +0300

    configuration for database-main-model dependencies and first functioning post request to the web app server. Also adds single stock data to database.

--- database.py
+++ database.py
@@ -1,32 +1,49 @@
 from sqlalchemy import Column, create_engine, Table, select, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
-# from sqlalchemy.orm import sessionmaker -> when taking ORM into use, use sessions
+
 import time
 import logging
 
-print('Kuinka tätä ajetaan')
-# Database with stockdata
+# Log in credentials and connection URL-string as DATABASE_CONNECTION
+load_dotenv()
+SERVER_IP = os.getenv("SERVER_IP")
+DRIVER = os.getenv("DRIVER")
+DATABASE = os.getenv("DATABASE")
+UID = os.getenv("UID")
+PWD = os.getenv("PWD")
+DATABASE_CONNECTION = f"mssql+pyodbc://{UID}:{PWD}@{SERVER_IP}/{DATABASE}?driver={DRIVER}"
+
+# Engine and sessionmaker
+engine = create_engine(DATABASE_CONNECTION, echo=True) # Creating an engine object to connect to database
+metadata = MetaData()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Declarative base
+Base = declarative_base()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Populate database with stockdata
 def save_to_database(stock_data_list):
-    load_dotenv()
-    metadata = MetaData()
-
-    # Log in credentials and connection URL-string as DATABASE_CONNECTION
-    SERVER_IP = os.getenv("SERVER_IP")
-    DRIVER = os.getenv("DRIVER")
-    DATABASE = os.getenv("DATABASE")
-    UID = os.getenv("UID")
-    PWD = os.getenv("PWD")
-    DATABASE_CONNECTION = f"mssql+pyodbc://{UID}:{PWD}@{SERVER_IP}/{DATABASE}?driver={DRIVER}"
-
+    print("Save to database in action...")
     # Set up logging configuration
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         filename='database.log',
                         filemode='a')
-
-    # Creating an engine object to connect to database
-    engine = create_engine(DATABASE_CONNECTION)
 
     # Using the engine object for straight connection to DB
     connection = engine.connect()
@@ -41,22 +58,35 @@ def save_to_database(stock_data_list):
     # SQL query with SQLAlchemy methods
     if not metadata.tables.get('stock_data'):
         # Create the table if it doesn't exist
-        stock_data_table.create()
+        stock_data_table = metadata.create_all()
         print("Table 'stock_data' created")
 
-        for stock_data in stock_data_list:
-            stock_data_table.insert().values(
-                name=stock_data['name'],
-                price=str(stock_data['price']),
-                volume=str(stock_data['volume']),
-                previous_close_price=str(stock_data['previous_close_price'])
-            ).execute()
-    else:
-        for stock_data in stock_data_table:
-            print(stock_data)
-        
-
-    time.sleep(1)
-
+    for stock_data in stock_data_list:
+        connection.execute(stock_data_table.insert().values(
+            name=stock_data['name'],
+            price=str(stock_data['price']),
+            volume=str(stock_data['volume']),
+            previous_close_price=str(stock_data['previous_close_price'])
+        ))
     print("Closing connection...")
     connection.close()
+
+# CREATE function, add new stock to portfolio, portfolio 1 - N stocks
+def create_portfolio_item():
+    return
+
+# READ functions, read stockmarket and portfolio as different tabs
+def get_portfolio():
+    return
+def get_stockmarket():
+    return
+
+# UPDATE function, if needed to add/buy more existing stock
+def update_portfolio_item():
+    return
+
+# DELETE function, delete from portfolio
+def delete_portfolio_item():
+    return
+
+
--- models.py
+++ models.py
@@ -1,18 +1,31 @@
+
 from sqlalchemy import Boolean, Column, Integer, String
-from database import base
+from database import Base
 
-
-class Stock(base):
-    __tablename__ = 'stocks'
+class Stock(Base):
+    __tablename__ = 'stock_data'
     id = Column(Integer, primary_key=True, index=True)
-    ticker = Column(String(10))
+    ticker = Column(String(10), unique=True)
     name = Column(String(50))
     price_today = Column(Integer)
-    Last_days_price = Column(Integer)
-    Volume = Column(Integer)
+    last_days_price = Column(Integer)
+    volume = Column(Integer)
+    #date
+
+from pydantic import BaseModel, Field
+############ pydantic mallien alustaminen -> varmentaa
+class StockBase(BaseModel):
+    ticker: str
+    name: str
+    price_today: int
+    last_days_price: int
+    volume: int
     
-class salkku(base):
-    __tablename__ = 'salkku'
-    id = Column(Integer, primary_key= True, index=True)
-    stocks = Column(Integer, Integer)
-    Value = Column(Integer)
+
+# class Portfolio(Base):
+#     __tablename__ = 'salkku'
+#     id = Column(Integer, primary_key=True, index=True)
+#     stocks = Column(String)
+#     value = Column(Integer)
+
+
