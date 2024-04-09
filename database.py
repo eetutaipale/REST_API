69a6d953f275493b6bf64c6a79c5657b127be9b2
unknown <eetaip@utu.fi>
2024-04-09 10:07:09 +0300

    Revert "Revert "Revert "Revert "väärin toimi""""
    This reverts commit 3cefcc2746dd558f1ec421752ad741fa25fd376b.

--- database.py
+++ database.py
@@ -88,12 +88,6 @@ def save_to_database(stock_data_list):
         ))
     print("Closing connection...")
     connection.close()
-def generate_id(db):
-    # Query the length of the Stock table
-    table_length = db.query(func.count(models.Stock.id)).scalar()
-    # Generate ID based on the length of the table
-    print(table_length)
-    return table_length + 1
 
 # CREATE function, add new stock to portfolio, portfolio 1 - N stocks
 def create_portfolio_item():
