fd9932a0c1ef9a523fc5563a72214798bcfc1b9f
Arttu Kuitunen <arsaku@utu.fi>
2024-04-18 13:20:08 +0300

    asyncs to all endpoint functions

--- main.py
+++ main.py
@@ -16,14 +16,15 @@ load_dotenv()
 app = FastAPI()
 
 
-# This try-clause ought to be better placed
+
+# Maybe change into a method ie. def create_all_tables()
 try:
     database.Base.metadata.create_all(bind=engine)
     print("Creating all tables from models to database")
 except Exception as e:
     print("Error: ", e)
 
-# Database dependency method
+# Database dependency method to start a db_session
 def get_db():
     print("Creating session in get_db()")
     db = SessionLocal()
@@ -33,7 +34,7 @@ def get_db():
     finally:
         db.close()
 
-# Checks the amout of models.Stock -type items in database and returns next value as int
+# Creates an id by checking the amout of models.Stock -type items in database and returns next value as int
 def generate_id(db: Session) -> int:
     # Query the length of the Stock table
     table_length = db.query(models.Stock).count()
@@ -42,13 +43,11 @@ def generate_id(db: Session) -> int:
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-
-
 ######################    
-# FastAPI endpoints  to PORTFOLIO
-# Endpoint to populate the database
+# FastAPI ENDPOINTS to communicate with client/frontend
+
 @app.post("/populate_database")
-def populate_database(db: Session = Depends(get_db)):
+async def populate_database(db: Session = Depends(get_db)):
     try:
         stock_data_list = fetch_api_data()
         for stock_data in stock_data_list:
@@ -61,10 +60,9 @@ def populate_database(db: Session = Depends(get_db)):
         db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
     
-
 # Post toimii 
 @app.post("/stock/")
-def create_stock(stock: models.StockBase, db: Session = Depends(get_db)):
+async def create_stock(stock: models.StockBase, db: Session = Depends(get_db)):
     try:
         print("Trying to post("")")
         db_stock = models.Stock(**stock.model_dump())
@@ -76,21 +74,24 @@ def create_stock(stock: models.StockBase, db: Session = Depends(get_db)):
         db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
     
+
 @app.get("/stock/")
-def get_stock(db: Session = Depends(get_db)):
+async def get_stock(db: Session = Depends(get_db)):
     
     stock_data = db.query(models.Stock).all()
     if not stock_data:
         raise HTTPException(status_code=404, detail="Stock_data not found")
     return stock_data
 
+
 @app.get("/stock/{stock_id}")
-def get_stock(stock_id: int, db: Session = Depends(get_db)):
+async def get_stock(stock_id: int, db: Session = Depends(get_db)):
     stock = db.query(models.Stock).filter(models.Stock.id == stock_id).first()
     
     if not stock:
         raise HTTPException(status_code=404, detail="Stock not found")
     return stock
+
 
 @app.put("//{id}/update-volume/")
 async def update_volume(id: int, volume: int, db: Session = Depends(get_db)):
@@ -102,7 +103,6 @@ async def update_volume(id: int, volume: int, db: Session = Depends(get_db)):
     db.refresh(stock)
     return {"message": "Volume updated successfully", "ticker": id, "new_volume": volume}
 
-
 # Portfolio calls
 @app.post("/portfolio/post")
 async def create_portfolio_item(portfolio_value: int, db: Session = Depends(get_db)):
@@ -111,10 +111,12 @@ async def create_portfolio_item(portfolio_value: int, db: Session = Depends(get_
     db.commit()
     return {"message": "new transaction done"}
 
+
 @app.get("/portfolios/")
 async def get_portfolios(db: Session = Depends(get_db)): 
     portfolios = db.query(models.Portfolio).all()
     return portfolios
+
 
 @app.put("/portfolio/{id}")
 async def update_portfolio_item(portfolio_id: int, portfolio_value: int, db: Session = Depends(get_db)):
@@ -125,6 +127,7 @@ async def update_portfolio_item(portfolio_id: int, portfolio_value: int, db: Ses
         return portfolio_item
     else:
         raise HTTPException(status_code=404, detail="Portfolio item not found")
+
 
 @app.delete("/portfolio/{id}")
 async def delete_portfolio_item(portfolio_id: int, db: Session = Depends(get_db)):
